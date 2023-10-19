9691e23884fe50c2d6b99fc6c77d7c4fb241c1e5
infiduk <infi_duk@naver.com>
2023-10-20 01:21:15 +0900

    :beers: test: update web3Auth test code

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import {
   Box,
+  Button,
   Grid,
   Text,
 } from '@chakra-ui/react';
@@ -11,65 +12,59 @@ import { ColorModeSwitcher } from '../ColorModeSwitcher';
 import { OrderCardList } from './OrderCardList';
 import { Web3AuthModalPack } from '@safe-global/auth-kit';
 
-const adapterSettings = {
-  uxMode: "popup",
-  whiteLabel: {
-    appName: "eAAts",
-    appUrl: "https://web3auth.io",
-    logoLight: "https://web3auth.io/images/w3a-L-Favicon-1.svg",
-    logoDark: "https://web3auth.io/images/w3a-D-Favicon-1.svg",
-    defaultLanguage: "ko", // en, de, ja, ko, zh, es, fr, pt, nl
-    mode: "dark", // whether to enable dark mode. defaultValue: auto
-    theme: {
-      primary: "#00D1B2",
-    },
-    useLogoLoader: true,
-  }
-}
 const options = {
-  clientId: '123',
-  web3AuthNetwork: 'mainnet',
+  clientId: '12345',
+  web3AuthNetwork: 'testnet',
   chainConfig: {
     chainNamespace: CHAIN_NAMESPACES.EIP155,
-    chainId: '0x89',
-    rpcTarget: 'https://polygon-rpc.com'
+    chainId: '0x458',
+    rpcTarget: 'https://api.test.wemix.com'
   }
 }
 
 export const Order = () => {
-  const [web3Pack, setWeb3Pack] = useState();
-  const [address, setAddress] = useState();
+  const [web3Pack, setWeb3Pack] = useState(null);
+  const [address, setAddress] = useState("");
+  const [userInfo, setUserInfo] = useState({});
 
   useEffect(() => {
-    initWeb3AuthModal()
+    initWeb3AuthModal();
+    checkLogin();
   }, [])
 
-  useEffect(() => {
-    console.log("web3Pack", web3Pack);
-  }, [web3Pack])
-
-  useEffect(() => {
-    console.log("eoa", address);
-  }, [address])
-
   const initWeb3AuthModal = async () => {
-    const openloginAdapter = new OpenloginAdapter({
-      adapterSettings
-    })
-    console.log(openloginAdapter)
+    try {
+      // 인스턴스 생성
+      const web3AuthModalPack = new Web3AuthModalPack({
+          txServiceUrl: 'https://safe-transaction-mainnet.safe.global' // safe 관련
+      })
+      // 옵션 파람스 https://web3auth.io/docs/sdk/pnp/web/modal/initialize#instantiating-web3auth
+      await web3AuthModalPack.init({ options })
 
-   const web3AuthModalPack = new Web3AuthModalPack({
-      txServiceUrl: 'https://safe-transaction-mainnet.safe.global'
-   })
-    await web3AuthModalPack.init({ options }, [openloginAdapter])
-    setWeb3Pack(web3AuthModalPack)
+      // 이벤트 구독
+      web3AuthModalPack.subscribe("connected", (e) => console.log("connect!", e));
+      web3AuthModalPack.subscribe("connecting", (e) =>  console.log("connecting...", e));
+      web3AuthModalPack.unsubscribe("disconnected", (e) => console.log("disconnect", e));
 
-    web3AuthModalPack.subscribe("connected", (e) => console.log("connect!", e));
-    web3AuthModalPack.subscribe("connecting", (e) => console.log("connecting...", e));
-    web3AuthModalPack.unsubscribe("disconnected", (e) => console.log("disconnect", e));
+      setWeb3Pack(web3AuthModalPack)
+    } catch (e) {
+      console.error("initWeb3AuthModal:error", e);
+    }
   }
 
-  const login = async () => {
+  // ! 상태관리
+  // 새로고침 시 데이터 날아가서 가져옴
+  const checkLogin = () => {
+    const address = window.localStorage.getItem("address")
+    const userInfo = window.localStorage.getItem("openlogin_store");
+    if(address === null) return;
+    if(userInfo === null) return;
+
+    setAddress(address);
+    setUserInfo(JSON.parse(userInfo))
+  }
+
+  const login = async () => {  
     if (web3Pack === undefined) {
       alert("plz web3AuthModalPack init");
       return;
@@ -82,34 +77,57 @@ export const Order = () => {
 
     try {
       const { eoa, safes } = await web3Pack.signIn();
-      setAddress(eoa);
+      // 소셜 로그인 정보
       const userInfo = await web3Pack.getUserInfo();
       const web3Provider = await web3Pack.getProvider();
-      console.log(eoa, safes, userInfo, web3Provider);
+
+      window.localStorage.setItem("address", eoa);
+      setAddress(eoa);
+      setUserInfo(userInfo)
     } catch (e) {
-      console.error("error", e)
+      console.error("login:error", e)
     }
   }
 
   const logout = async () => {
     if (web3Pack === undefined) {
-      alert("You are not logged in :(");
+      alert("plz web3AuthModalPack init");
       return;
     }
     const { status } = web3Pack.web3Auth;
-    if (status === "ready") {
+    if (status === "ready" || status === "connecting") {
       alert("You are not logged in :(");
       return;
     }
 
+    try {
      await web3Pack.signOut();
+
+     window.localStorage.removeItem("address");
+     setAddress("");
+     setUserInfo({});
+    } catch (e) {
+      console.error("logout:error", e);
+    }
+  }
+
+  const sendTransaction = () => {
+
   }
 
   return (
     <Box textAlign="center" fontSize="xl">
-      <button onClick={() => login()}>login</button>
-      <button onClick={() => logout()}>logout</button>
+      <Button onClick={() => login()}>login</Button>
+      <Button onClick={() => logout()}>logout</Button>
+      <Button onClick={() => sendTransaction()}>sendTransaction</Button>
       <Text>address: {address}</Text>
+      {Object.keys(userInfo).length > 0 &&
+        <>
+          <Text>email: {userInfo.email}</Text>
+          <Text>name: {userInfo.name}</Text>
+          <Text>typeOfLogin: {userInfo.typeOfLogin}</Text>
+        </>
+      }
       <Grid minH="100vh" p={5} px={10}>
         <ColorModeSwitcher justifySelf="flex-end" />
         <OrderCardList />
