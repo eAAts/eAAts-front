6937ce7ca3d338ea27ea82be3d2ce48fac5ed5e0
infiduk <infi_duk@naver.com>
2023-10-20 17:59:43 +0900

    :warning: test: change test code chain - polygon mainnet

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -4,10 +4,7 @@ import {
   Button,
   Grid,
   HStack,
-  Stack,
   Text,
-  VStack,
-  useCheckboxGroup,
 } from '@chakra-ui/react';
 import { CHAIN_NAMESPACES, WALLET_ADAPTERS } from '@web3auth/base'
 import { OpenloginAdapter } from "@web3auth/openlogin-adapter";
@@ -16,25 +13,19 @@ import { ColorModeSwitcher } from '../ColorModeSwitcher';
 import { OrderCardList } from './OrderCard';
 import { Web3AuthModalPack } from '@safe-global/auth-kit';
 import Web3 from 'web3';
-import Gov from '../gov.json'
+import eAAts from '../eAAts.json'
 import { OrderCheckbox } from './OrderCheckbox';
 
 const options = {
-  clientId: 'BPi5PB_UiIZ-cPz1GtV5i1I2iOSOHuimiXBI0e-Oe_u6X3oVAbCiAZOTEBtTXw4tsluTITPqA8zMsfxIKMjiqNQ',
-  web3AuthNetwork: 'testnet',
+  clientId: 'BL7DvTW5eDnbbseTHotSiB8fFhueObVhMknseYNi4ICU8am0tB_6FWF-KU3i3gNjM5_IWs-mSNvBQEYSFTVB3AU',
+  web3AuthNetwork: 'mainnet',
   chainConfig: {
     chainNamespace: CHAIN_NAMESPACES.EIP155,
-    chainId: "0x13881",
-    displayName: "Mumbai",
-    blockExplorer: "",
-    ticker: "MATIC",
-    tickerName: "MATIC",
-    rpcTarget: "https://matic-mumbai.chainstacklabs.com",
+    chainId: "0x89",
+    blockExplorer: "https://mumbai.polygonscan.com",
+    rpcTarget: "https://rpc-mainnet.maticvigil.com",
   },
-  uiConfig: {
-    theme: 'light',
-    loginMethodsOrder: ['facbook', 'google']
-  }
+  authMode: "DAPP"
 }
 const modalConfig = {
   [WALLET_ADAPTERS.TORUS_EVM]: {
@@ -63,15 +54,15 @@ export const Order = () => {
     try {
       // 인스턴스 생성
       const web3AuthModalPack = new Web3AuthModalPack({
-          txServiceUrl: 'https://safe-transaction-mainnet.safe.global' // safe 관련
+          txServiceUrl: 'https://safe-transaction-polygon.safe.global' // safe 관련
       })
       // 옵션 파람스 https://web3auth.io/docs/sdk/pnp/web/modal/initialize#instantiating-web3auth
-      await web3AuthModalPack.init({ options }, [], modalConfig);
+      await web3AuthModalPack.init({ options }, [], modalConfig );
 
       // 이벤트 구독
       web3AuthModalPack.subscribe("connected", (e) => console.log("connect!", e));
       web3AuthModalPack.subscribe("connecting", (e) =>  console.log("connecting...", e));
-      web3AuthModalPack.unsubscribe("disconnected", (e) => console.log("disconnect", e));
+      web3AuthModalPack.subscribe("disconnected", (e) => console.log("disconnect", e));
 
       setWeb3Pack(web3AuthModalPack)
     } catch (e) {
@@ -106,13 +97,13 @@ export const Order = () => {
       const { eoa, safes } = await web3Pack.signIn();
       // 소셜 로그인 정보
       const userInfo = await web3Pack.getUserInfo();
-      console.log(userInfo)
+      console.log(eoa, safes, userInfo)
       // web3 객체 만들기
-      const web3Provider = await web3Pack.getProvider();
-      const web3 = new Web3(web3Provider);
+      // const web3Provider = await web3Pack.getProvider();
+      // const web3 = new Web3(web3Provider);
 
       window.localStorage.setItem("address", eoa);
-      setWeb3(web3);
+      // setWeb3(web3);
       setAddress(eoa);
       setUserInfo(Object.keys(userInfo).length ? userInfo : null)
     } catch (e) {
@@ -142,6 +133,24 @@ export const Order = () => {
     }
   }
 
+  // const waitForReceipt = (hash: string, cb: Function) => {
+  //   // console.log('Start waitForReceipt: ', hash)
+  //   provider.httpWeb3.eth.getTransactionReceipt(hash, (err, receipt) => {
+  //     // console.log('getTransactionReceipt: ', receipt)
+  //     if (err) console.log("err: ", err);
+
+  //     if (receipt === undefined || receipt === null) {
+  //       // Try again in 1 second
+  //       window.setTimeout(() => {
+  //         waitForReceipt(hash, cb);
+  //       }, 1000);
+  //     } else {
+  //       // Transaction went through
+  //       if (cb) cb(receipt);
+  //     }
+  //   });
+  // };
+
   const sendTransaction = async () => {
     if (web3 === null) {
       alert("Failed :(");
@@ -149,14 +158,20 @@ export const Order = () => {
     }
     try {
       // contract 객체 만들기
-      const contract = new web3.eth.Contract(Gov.abi, "0xa98C15B5D0d4F12A7b4C12bb8FA216f446D6534D")
-      // sign
-      await web3.eth.personal.sign("1234", address)
+      const contract = new web3.eth.Contract(eAAts.abi, "0xBB97CcD6EAB2891eac05A67181aa45f7e8a84c3C")
+      console.log(contract);
+      // * sign
+      // await web3.eth.personal.sign("1234", address)
+      // * list
+      // const list = await contract.methods.getOrdersByStatus(0).call();
       // send transaction
-      const encodeABI = contract.methods.finalizeEndedVote().encodeABI();
+      // * create order
+      const encodeABI = contract.methods.createOrder(100, 0).encodeABI();
+      // * join order
+      // const encodeABI = contract.methods.joinOrder(1, 2).encodeABI();
       web3.eth.sendTransaction({
         from: address,
-        to: "0xa98C15B5D0d4F12A7b4C12bb8FA216f446D6534D",
+        to: "0xBB97CcD6EAB2891eac05A67181aa45f7e8a84c3C",
         data: encodeABI,
         gasPrice: 101000000000,
         value: "0x0",
